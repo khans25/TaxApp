c8ef7fa9fce0d019c77b6bc464b44bae8897d57e
Saif Khan <Saif Khan>
2017-07-31 12:19:28 -0400

    Add project files.

---
+++ Module 5and6Solution/Tax.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_5and6Solution
+{
+    class Tax
+    {
+        private int ID;
+        private char name;
+        private double monSal;
+        private double fedTaxPer;
+        private double stateTaxPer;
+        private double monFedAmt;
+        private double monStateAmt;
+        private double monSalNet;
+        private int payPer;
+        private string aPaycheck;
+
+        public Tax() { }
+
+
+        //add constructor
+        public Tax(int employeeID, char employeeName, double monthlySalary, int payPeriods)
+        {
+            ID = employeeID;
+            name = employeeName;
+            monSal = monthlySalary;
+            payPer = payPeriods;
+            DetermineTaxBracket(monthlySalary);
+            DetermineCheckAmount(fedTaxPer, stateTaxPer);
+            aPaycheck = OutputData();
+
+        }
+
+
+
+        public double MonFedAmt
+        {
+            get
+            {
+                return monFedAmt;
+            }
+        }
+
+        public double MonStateAmt
+        {
+            get
+            {
+                return monStateAmt;
+            }
+        }
+
+        public double MonSalNet
+        {
+  
+          get
+            {
+                return monSalNet;
+            }
+        }
+
+
+        public void DetermineTaxBracket(double monSal)
+        {
+            if ((monSal * 12) <= 10000)
+            {
+                fedTaxPer = 0.10;
+                stateTaxPer = 0.03;
+            }
+            else if ((monSal * 12) <= 37000)
+            {
+                fedTaxPer = 0.15;
+                stateTaxPer = 0.05;
+            }
+            else if ((monSal * 12)<= 91000)
+            {
+                fedTaxPer = 0.25;
+                stateTaxPer = 0.07;
+            }
+            else if ((monSal * 12) >= 91000)
+            {
+                fedTaxPer = 0.30;
+                stateTaxPer = 0.09;
+            }
+
+        }
+
+        public void DetermineCheckAmount(double fedTaxPer, double stateTaxPer)
+        {
+            monFedAmt = fedTaxPer * monSal;
+            monStateAmt = stateTaxPer * monSal;
+            monSalNet = monSal - (monFedAmt - monStateAmt);
+        }
+
+        public string OutputData()
+        {
+            string aP = "Check #\tGross Wages\tState Taxes\tFederal Taxes\tNet\n";
+            double payPd, mSS = 0.0, mFAS = 0.0, mSAS = 0.0, mSNS = 0.0;
+            for (payPd = 1; payPd <=payPer;  payPd++)
+            {
+                aP += payPd + "\t" + monSal.ToString("C") + "\t" + monFedAmt.ToString("C") + "\t" + 
+                    monStateAmt.ToString("C") + "\t" + monSalNet.ToString("C") + "\n";
+                mSS += monSal;
+                mFAS += monFedAmt;
+                mSAS += monStateAmt;
+                mSNS += monSalNet;
+            }
+
+            aP += payPd + "\t" + mSS.ToString("C") + "\t" + mFAS.ToString("C") + "\t" +
+                              mSAS.ToString("C") + "\t" + mSNS.ToString("C") + "\n";
+            return aP;
+        }
+        public string APaycheck
+        {
+            get
+            {
+                return aPaycheck;
+            }
+        }
+        public override string ToString()
+        {
+            return payPer.ToString() + monSal.ToString("C") + "\n" + aPaycheck;
+        }
+    }
+
+
+}
---
+++ Module 5and6Solution/TaxApp.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_5and6Solution
+{
+    class TaxApp
+    {
+        static void Main(string[] args)
+        {
+            int employeeID;
+            char employeeName;
+            double monthlySalary;
+            int payPeriods;
+            string inValue;
+            char anotherEmployee = 'N';
+            DisplayInstructions();
+            ReadKey();
+            do
+            {
+                GetInputValues(out employeeID, out employeeName, out monthlySalary, out payPeriods);
+                Tax ln = new Tax(employeeID, employeeName, monthlySalary, payPeriods);
+                WriteLine();
+                Clear();
+                WriteLine(ln);
+                WriteLine();
+                WriteLine(ln.OutputData());
+                WriteLine("\nWould you like to enter another employee? (Y or N)");
+                inValue = ReadLine();
+                anotherEmployee = Convert.ToChar(inValue);
+            }
+
+            while ((anotherEmployee == 'Y') || (anotherEmployee == 'y'));
+        }
+
+        static void DisplayInstructions()
+        {
+            WriteLine("TAX APPLICATION" +
+                "\n\nThis application will ask you to input the Employee ID" +
+                "\n Employee Name, Monthly Salary, Federal and State taxes (percentages)" +
+                "\nnumber of pay periods per year" + 
+                "\n\nPRESS ANY KEY TO CONTINUE");
+        }
+
+        static void GetInputValues(out int employeeID, out char employeeName, out double monthlySalary, out int payPeriods)
+        {
+            Clear();
+            employeeID = ObtainEmployeeID();
+            employeeName = ObtainEmployeeName();
+            monthlySalary = ObtainMonthlySalary();
+            payPeriods = ObtainPayPeriods();
+        }
+
+        public static int ObtainEmployeeID()
+        {
+            string stringValue;
+            int employeeID;
+            Write("Enter the Employee ID: ");
+            stringValue = ReadLine();
+            while ((int.TryParse(stringValue, out employeeID) == false)
+                || employeeID < 0
+                || employeeID > 1000)
+
+            {
+                Write("\nInvalid Employee ID entered.");
+                Write("\nRe-enter the Employee ID: ");
+                stringValue = ReadLine();
+            }
+
+            return employeeID;
+        }
+
+        public static char ObtainEmployeeName()
+        {
+            string stringValue;
+            char employeeName;
+            Write("Enter the Employee Name: ");
+            stringValue = ReadLine();
+            char.TryParse(stringValue, out employeeName);
+            
+            return employeeName;
+        }
+
+        public static double ObtainMonthlySalary()
+        {
+            string stringValue;
+            double monthlySalary;
+            Write("Enter the monthly salary: ");
+            stringValue = ReadLine();
+            while ((double.TryParse(stringValue, out monthlySalary) == false)
+                || monthlySalary < 0
+                || monthlySalary > 1000000)
+
+            {
+                Write("\nInvalid monthly entered.");
+                Write("\nRe-enter the monthly salary ");
+                stringValue = ReadLine();
+            }
+
+            return monthlySalary;
+        }
+
+       /* public static int ObtainFedTaxPercent()
+        {
+            string stringValue;
+            int fedTaxPercent;
+            Write("Enter the Federal Tax Percentage: ");
+            stringValue = ReadLine();
+            while ((int.TryParse(stringValue, out fedTaxPercent) == false)
+                || fedTaxPercent < 0
+                || fedTaxPercent > 50)
+
+            {
+                Write("\nInvalid Federal Tax Percentage entered.");
+                Write("\nRe-enter the Federal Tax Percentage: ");
+                stringValue = ReadLine();
+            }
+
+            return fedTaxPercent;
+        }
+
+        public static int ObtainStateTaxPercent()
+        {
+            string stringValue;
+            int stateTaxPercent;
+            Write("Enter the State Tax Percent: ");
+            stringValue = ReadLine();
+            while ((int.TryParse(stringValue, out stateTaxPercent) == false)
+                || stateTaxPercent < 0
+                || stateTaxPercent > 100)
+
+            {
+                Write("\nInvalid State Tax Percent.");
+                Write("\nRe-enter the State Tax Percent: ");
+                stringValue = ReadLine();
+            }
+
+            return employeeID;
+        }
+        */
+
+        public static int ObtainPayPeriods()
+        {
+            string stringValue;
+            int payPeriods;
+            Write("Enter the number of pay periods per year: ");
+            stringValue = ReadLine();
+            while ((int.TryParse(stringValue, out payPeriods) == false)
+                || payPeriods < 1
+                || payPeriods > 500)
+
+            {
+                Write("\nInvalid number of pay periods entered.");
+                Write("\nRe-enter the number of pay periods per year: ");
+                stringValue = ReadLine();
+            }
+
+            return payPeriods;
+        }
+    }
+}
+
